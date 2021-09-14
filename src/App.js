0f66413b38f96c0ff3b1e2c6c903db2c6b90896f
hlp <865225435@qq.com>
2021-09-14 18:09:58 +0800

    基本样式、图标引入

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
-
+import { IconStyle } from './assets/iconfont/iconfont'; // 引入图标
+import { GlobalStyle } from './style'; // 全局基本样式
 
 function App() {
   return (
     <div className="App">
-      
+      <GlobalStyle />
+      <IconStyle />
+      <i className="iconfont">&#xe644;</i>
     </div>
   );
 }
